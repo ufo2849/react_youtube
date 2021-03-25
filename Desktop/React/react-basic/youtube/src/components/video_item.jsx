32b9742999e4abe4841d024f0f540718c5e16a75
Jason Han <kyubumh@uci.edu>
2021-03-24 18:21:32 -0700

    make video item

--- Desktop/React/react-basic/youtube/src/components/video_item.jsx
+++ Desktop/React/react-basic/youtube/src/components/video_item.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
 const Videoitem = (props) => (
-    <h1>{props.video.snippet.title}</h1>
+    <li className={styles.container}>
+        <div className={styles.video}>
+            <img src={props.video.snippet.thumbnails.default.url} className={styles.thumb} alt="video_thumb"></img>
+            <div className={styles.description}>
+                <p className={styles.title}>{props.video.snippet.title}</p>
+                <p className={styles.channel}>{props.video.snippet.channelTitle}</p>
+            </div>
+        </div>
+    </li>
     );
 
 export default Videoitem;
--- Desktop/React/react-basic/youtube/src/components/video_list.jsx
+++ Desktop/React/react-basic/youtube/src/components/video_list.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Videoitem from './video_item';
+import styles from './video_list.module.css';
 
 const Videolist = (props) => (
-    <ul>
+    <ul className={styles.videos}>
         {props.videos.map(video => (
             <Videoitem key= {video.id} video= {video}/>
         ))}
